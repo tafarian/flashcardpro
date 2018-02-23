09a74ce54659284d1fa87c08ea210b32f5a5bd1e
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-23 14:36:09 +0100

    setting the stack and binding action creators

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import stacks from './../data/stacks';
+import { setStack } from './../actions';
 
 class StackList extends Component {
     render() {
+        console.log('stacklist props', this.props);
+
+
         return (
             <div>
                 {
                     stacks.map(stack => {
                         return (
-                            <Link to='/stack'>
-                                <h4 key={ stack.id }>{ stack.title }</h4>
+                            <Link 
+                            to='/stack' 
+                            key={ stack.id } 
+                            onClick={() => this.props.setStack(stack)}
+                            >
+                                <h4>{ stack.title }</h4>
                             </Link>
                         )
                     })
@@ -20,4 +29,4 @@ class StackList extends Component {
     }
 }
 
-export default StackList;
+export default connect(null, { setStack })(StackList);
