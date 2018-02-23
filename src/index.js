bf7dcb498dde2fb494c51c8272594ceb6863c0f0
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-23 14:23:33 +0100

    creating the redux store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import App from './components/App';
-import Stack from './components/Stack'
+import Stack from './components/Stack';
+import { setStack } from './actions';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(setStack({ id: 0, title: 'example', cards: [] }));
 
 ReactDOM.render(
-    <BrowserRouter> 
-        <Switch>
-            <Route exact path='/' component={ App }/>
-            <Route path='/stack' component={ Stack }/>
-        </Switch>
-    </BrowserRouter>,
+    <Provider store={ store }>
+        <BrowserRouter> 
+            <Switch>
+                <Route exact path='/' component={ App }/>
+                <Route path='/stack' component={ Stack }/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
  document.getElementById('root')
 );
