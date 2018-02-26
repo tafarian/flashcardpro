4edf2f9e6c3c105a152e9e72e5aee4477a3a2594
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-26 12:23:11 +0100

    loading stacks

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -27,7 +27,7 @@ class Stack extends Component {
 }
 
 function mapStateToProps(state) {
-    return { stack: state };
+    return { stack: state.stack };
 }
 
 export default connect(mapStateToProps, null)(Stack);
